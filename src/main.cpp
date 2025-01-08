1167f0d6e2133a707a2b70f4c6c102f349bd73f0
Egevtech <egevtech@gmail.com>
2025-01-09 01:37:24 +0300

    v0.5.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <string>
 
 void list_dir(const char* path);
 void list_file(const char* path);
 
-int main(int argc, char* argv[]) {
+    bool showLineNumbers = false;
+
+int main(int argc, char** argv) {
+
     if (argc == 1) { 
         std::cout << "[?" << std::filesystem::absolute(std::filesystem::current_path()) << "]\n";
         list_dir(std::filesystem::current_path().c_str()); return 0; 
-        }
+    }
 
     for (int i = 0; i < argc; i++) {
+        
+        std::string arg = argv[i];
+
+        if (arg.length() == 2 && arg[0] == '-') {
+            switch (arg[1]) {
+                case 'p': {
+                    std::cout << std::filesystem::absolute(std::filesystem::current_path()) << '\n';
+                    continue; 
+                } break;
+
+                case 'n': {
+                    showLineNumbers = true;
+                } break;
+            }
+        }
+
         if (!std::filesystem::exists(argv[i])) continue;
+
 
         std::cout << "[?" << std::filesystem::absolute(argv[i]) << "]\n";
 
@@ -39,8 +60,12 @@ void list_file(const char* path) {
 
     while (!file.eof()) {
         file.getline(buf, 1024);
-        std::cout << "    | " << buf << '\r';
-        std::cout << (++n >= 10000 ? (std::to_string((int)(n/1000)) + 'k') : std::to_string(n).c_str()) << '\n';
+        if (showLineNumbers) {
+            std::cout << "    | " << buf << '\r';
+            std::cout << (++n >= 10000 ? (std::to_string((int)(n/1000)) + 'k') : std::to_string(n).c_str()) << '\n';
+        } else {
+            std::cout << buf << '\n';
+        }
     }
     std::cout << '\n';
 }
